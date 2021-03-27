72d756144308adac3a6c35e75de4450eac90d5e0
Abderahmane Aoufi <abdouou7@gmail.com>
2021-03-27 20:56:14 +0100

    finish header , add router

---
+++ src/home/home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import styled from 'styled-components';
+
+function home() {
+      return (
+            <Container>
+                  Home
+            </Container>
+      )
+}
+
+export default home
+
+
+const Container = styled.div``;
