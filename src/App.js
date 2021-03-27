0149c92c098d89e876ca0c3ef7ebc7fff5baa376
Abderahmane Aoufi <abdouou7@gmail.com>
2021-03-27 20:56:34 +0100

    finish header add router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import "./App.css";
 import Header from "./components/Header/Header";
+import Cart from "./components/Cart/Cart";
+import Home from "../src/home/home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route path="/cart">
+            <Cart />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,6 +2,8 @@ import React from "react";
 import styled from "styled-components";
 import SearchIcon from "@material-ui/icons/Search";
 import ShopIcon from "@material-ui/icons/Shop";
+import LocationOnIcon from "@material-ui/icons/LocationOn";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
@@ -11,15 +13,20 @@ function Header() {
           <img src={"https://i.imgur.com/7I9Was5.png"} alt="logo" />
         </HeaderLogo>
         <HeaderOptionAdress>
-          <OptionLineOne>Hello </OptionLineOne>
-          <OptionLineTwo>Select your adress</OptionLineTwo>
+          <LocationOnIcon />
+          <HeaderOption>
+            <OptionLineOne>Hello </OptionLineOne>
+            <OptionLineTwo>Select your adress</OptionLineTwo>
+          </HeaderOption>
         </HeaderOptionAdress>
+
         <HeaderSearch>
           <HeaderSearchInput type="text" />
           <HeaderSearchIconContainer>
             <SearchIcon />
           </HeaderSearchIconContainer>
         </HeaderSearch>
+
         <HeaderNavItems>
           <HeaderOption>
             <OptionLineOne>Hello , nazzary</OptionLineOne>
@@ -28,12 +35,13 @@ function Header() {
 
           <HeaderOption>
             <OptionLineOne>Reuturns</OptionLineOne>
-            <OptionLineTwo>& Deliveries</OptionLineTwo>
+            <OptionLineTwo>& Orders</OptionLineTwo>
           </HeaderOption>
-
           <HeaderOptionCard>
-            <ShopIcon />
-            <CartCount>4</CartCount>
+            <Link to="/cart">
+              <ShopIcon />
+              <CartCount>4</CartCount>
+            </Link>
           </HeaderOptionCard>
         </HeaderNavItems>
       </Container>
@@ -49,6 +57,7 @@ const Container = styled.div`
   display: flex;
   align-items: center;
   color: white;
+  justify-content: space-between;
 `;
 const HeaderLogo = styled.div`
   img {
@@ -56,16 +65,69 @@ const HeaderLogo = styled.div`
     margin-left: 11px;
   }
 `;
-const HeaderOptionAdress = styled.div``;
+const HeaderOptionAdress = styled.div`
+  align-items: center;
+  display: flex;
+  padding-left: 9px;
+`;
+
 const OptionLineOne = styled.div``;
-const OptionLineTwo = styled.div``;
+
+const OptionLineTwo = styled.div`
+  font-weight: 700;
+`;
 const HeaderSearch = styled.div`
   display: flex;
+  flex-grow: 1;
+  height: 40px;
+  border-radius: 4px;
+  overflow: hidden;
+  margin-left: 4px;
+  background-color: white;
+  :focus-within {
+    box-shadow: 0 0 0 3px #f90;
+  }
 `;
-const HeaderSearchInput = styled.input``;
-const HeaderSearchIconContainer = styled.div``;
+const HeaderSearchInput = styled.input`
+  flex-grow: 1;
+  border: none;
+  :focus {
+    outline: none;
+  }
+`;
+
+const HeaderSearchIconContainer = styled.div`
+  background-color: #febd69;
+  width: 45px;
+  color: black;
+  display: grid;
+  align-items: center;
+  place-items: center;
+  cursor: pointer;
+`;
+
 const HeaderNavItems = styled.div`
-display : flex`;
-const HeaderOption = styled.div``;
-const HeaderOptionCard = styled.div``;
-const CartCount = styled.div``;
+  display: flex;
+`;
+const HeaderOption = styled.div`
+  padding-left : 9px ;
+  padding-right : 9px
+  pading-top : 10px;
+  padding-bottom : 10px
+  `;
+
+const HeaderOptionCard = styled.div`
+  display: flex;
+  align-items: center;
+  padding-right: 9px;
+  padding-left: 9px;
+  a{
+    display : flex;
+    text-decoration : none;
+    color : white;
+  }
+`;
+
+const CartCount = styled.div`
+  padding-left: 9px;
+`;
